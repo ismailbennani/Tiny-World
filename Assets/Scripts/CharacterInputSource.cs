5f483a03ec8b891190147b9724b1df8a1bdc1465
Ismail Bennani <avace.contact@gmail.com>
2022-11-20 11:23:52 +0100

    don't move camera with mouse input

--- Assets/Scripts/CharacterInputSource.cs
+++ Assets/Scripts/CharacterInputSource.cs
@@ -5,28 +5,15 @@ public class CharacterInputSource : MonoBehaviour
 {
 	[Header("Character Input Values")]
 	public Vector2 move;
-	public Vector2 look;
 	public bool jump;
 	public bool sprint;
 
 	[Header("Movement Settings")]
 	public bool analogMovement;
 
-	[Header("Mouse Cursor Settings")]
-	public bool cursorLocked = true;
-	public bool cursorInputForLook = true;
-
 	public void OnMove(InputValue value)
 	{
 		MoveInput(value.Get<Vector2>());
-	}
-
-	public void OnLook(InputValue value)
-	{
-		if(cursorInputForLook)
-		{
-			LookInput(value.Get<Vector2>());
-		}
 	}
 
 	public void OnJump(InputValue value)
@@ -39,15 +26,9 @@ public class CharacterInputSource : MonoBehaviour
 		SprintInput(value.isPressed);
 	}
 
-
 	public void MoveInput(Vector2 newMoveDirection)
 	{
 		move = newMoveDirection;
-	} 
-
-	public void LookInput(Vector2 newLookDirection)
-	{
-		look = newLookDirection;
 	}
 
 	public void JumpInput(bool newJumpState)
@@ -58,15 +39,5 @@ public class CharacterInputSource : MonoBehaviour
 	public void SprintInput(bool newSprintState)
 	{
 		sprint = newSprintState;
-	}
-
-	private void OnApplicationFocus(bool hasFocus)
-	{
-		SetCursorState(cursorLocked);
-	}
-
-	private void SetCursorState(bool newState)
-	{
-		Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
 	}
 }
--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -24,7 +24,8 @@ public class ThirdPersonController : MonoBehaviour
 
     public AudioClip landingAudioClip;
     public AudioClip[] footstepAudioClips;
-    [Range(0, 1)] public float footstepAudioVolume = 0.5f;
+    [Range(0, 1)]
+    public float footstepAudioVolume = 0.5f;
 
     [Space(10)]
     [Tooltip("The height the player can jump")]
@@ -53,35 +54,18 @@ public class ThirdPersonController : MonoBehaviour
     [Tooltip("What layers the character uses as ground")]
     public LayerMask groundLayers;
 
-    [Header("Cinemachine")]
-    [Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
-    public GameObject cinemachineCameraTarget;
-
-    [Tooltip("How far in degrees can you move the camera up")]
-    public float topClamp = 70.0f;
-
-    [Tooltip("How far in degrees can you move the camera down")]
-    public float bottomClamp = -30.0f;
-
-    [Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
-    public float cameraAngleOverride = 0.0f;
-
-    [Tooltip("For locking the camera position on all axis")]
-    public bool lockCameraPosition = false;
-
     // cinemachine
-    private float _cinemachineTargetYaw;
     private float _cinemachineTargetPitch;
 
     // player
     private float _speed;
     private float _animationBlend;
-    private float _targetRotation = 0.0f;
+    private float _targetRotation;
     private float _rotationVelocity;
     private float _verticalVelocity;
     private float _terminalVelocity = 53.0f;
 
-    // timeout deltatime
+    // timeout delta time
     private float _jumpTimeoutDelta;
     private float _fallTimeoutDelta;
 
@@ -92,18 +76,12 @@ public class ThirdPersonController : MonoBehaviour
     private int _animIDFreeFall;
     private int _animIDMotionSpeed;
 
-    private PlayerInput _playerInput;
     private Animator _animator;
     private CharacterController _controller;
     private CharacterInputSource _characterInputSource;
     private GameObject _mainCamera;
 
-    private const float Threshold = 0.01f;
-
     private bool _hasAnimator;
-
-    private bool IsCurrentDeviceMouse => _playerInput.currentControlScheme == "KeyboardMouse";
-
 
     private void Awake()
     {
@@ -116,12 +94,11 @@ public class ThirdPersonController : MonoBehaviour
 
     private void Start()
     {
-        _cinemachineTargetYaw = cinemachineCameraTarget.transform.rotation.eulerAngles.y;
-            
+
         _hasAnimator = TryGetComponent(out _animator);
         _controller = GetComponent<CharacterController>();
         _characterInputSource = GetComponent<CharacterInputSource>();
-        _playerInput = GetComponent<PlayerInput>();
+        GetComponent<PlayerInput>();
 
         AssignAnimationIDs();
 
@@ -139,11 +116,6 @@ public class ThirdPersonController : MonoBehaviour
         Move();
     }
 
-    private void LateUpdate()
-    {
-        CameraRotation();
-    }
-
     private void AssignAnimationIDs()
     {
         _animIDSpeed = Animator.StringToHash("Speed");
@@ -156,37 +128,15 @@ public class ThirdPersonController : MonoBehaviour
     private void GroundedCheck()
     {
         // set sphere position, with offset
-        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - groundedOffset,
-                                             transform.position.z);
-        grounded = Physics.CheckSphere(spherePosition, groundedRadius, groundLayers,
-                                       QueryTriggerInteraction.Ignore);
+        Vector3 position = transform.position;
+        Vector3 spherePosition = new(position.x, position.y - groundedOffset, position.z);
+        grounded = Physics.CheckSphere(spherePosition, groundedRadius, groundLayers, QueryTriggerInteraction.Ignore);
 
         // update animator if using character
         if (_hasAnimator)
         {
             _animator.SetBool(_animIDGrounded, grounded);
         }
-    }
-
-    private void CameraRotation()
-    {
-        // if there is an input and camera position is not fixed
-        if (_characterInputSource.look.sqrMagnitude >= Threshold && !lockCameraPosition)
-        {
-            //Don't multiply mouse input by Time.deltaTime;
-            float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
-
-            _cinemachineTargetYaw += _characterInputSource.look.x * deltaTimeMultiplier;
-            _cinemachineTargetPitch += _characterInputSource.look.y * deltaTimeMultiplier;
-        }
-
-        // clamp our rotations so our values are limited 360 degrees
-        _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, bottomClamp, topClamp);
-
-        // Cinemachine will follow this target
-        cinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + cameraAngleOverride,
-                                                                      _cinemachineTargetYaw, 0.0f);
     }
 
     private void Move()
@@ -201,19 +151,18 @@ public class ThirdPersonController : MonoBehaviour
         if (_characterInputSource.move == Vector2.zero) targetSpeed = 0.0f;
 
         // a reference to the players current horizontal velocity
-        float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+        Vector3 velocity = _controller.velocity;
+        float currentHorizontalSpeed = new Vector3(velocity.x, 0.0f, velocity.z).magnitude;
 
         float speedOffset = 0.1f;
         float inputMagnitude = _characterInputSource.analogMovement ? _characterInputSource.move.magnitude : 1f;
 
         // accelerate or decelerate to target speed
-        if (currentHorizontalSpeed < targetSpeed - speedOffset ||
-            currentHorizontalSpeed > targetSpeed + speedOffset)
+        if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
         {
             // creates curved result rather than a linear one giving a more organic speed change
             // note T in Lerp is clamped, so we don't need to clamp our speed
-            _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
-                                Time.deltaTime * speedChangeRate);
+            _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * speedChangeRate);
 
             // round speed to 3 decimal places
             _speed = Mathf.Round(_speed * 1000f) / 1000f;
@@ -233,12 +182,10 @@ public class ThirdPersonController : MonoBehaviour
         // if there is a move input rotate player when the player is moving
         if (_characterInputSource.move != Vector2.zero)
         {
-            _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
-                              _mainCamera.transform.eulerAngles.y;
-            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
-                                                   rotationSmoothTime);
+            _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
 
-            // rotate to face input direction relative to camera position
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, rotationSmoothTime);
+
             transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
         }
 
@@ -246,8 +193,7 @@ public class ThirdPersonController : MonoBehaviour
         Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
 
         // move the player
-        _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
-                         new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+        _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
 
         // update animator if using character
         if (_hasAnimator)
@@ -326,37 +272,32 @@ public class ThirdPersonController : MonoBehaviour
         }
     }
 
-    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
-    {
-        if (lfAngle < -360f) lfAngle += 360f;
-        if (lfAngle > 360f) lfAngle -= 360f;
-        return Mathf.Clamp(lfAngle, lfMin, lfMax);
-    }
-
     private void OnDrawGizmosSelected()
     {
-        Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
-        Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
+        Color transparentGreen = new(0.0f, 1.0f, 0.0f, 0.35f);
+        Color transparentRed = new(1.0f, 0.0f, 0.0f, 0.35f);
 
-        if (grounded) Gizmos.color = transparentGreen;
-        else Gizmos.color = transparentRed;
+        Gizmos.color = grounded ? transparentGreen : transparentRed;
 
         // when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
-        Gizmos.DrawSphere(
-            new Vector3(transform.position.x, transform.position.y - groundedOffset, transform.position.z),
-            groundedRadius);
+        Vector3 position = transform.position;
+        Gizmos.DrawSphere(new Vector3(position.x, position.y - groundedOffset, position.z), groundedRadius);
     }
 
     private void OnFootstep(AnimationEvent animationEvent)
     {
-        if (animationEvent.animatorClipInfo.weight > 0.5f)
+        if (!(animationEvent.animatorClipInfo.weight > 0.5f))
         {
-            if (footstepAudioClips.Length > 0)
-            {
-                var index = Random.Range(0, footstepAudioClips.Length);
-                AudioSource.PlayClipAtPoint(footstepAudioClips[index], transform.TransformPoint(_controller.center), footstepAudioVolume);
-            }
+            return;
         }
+
+        if (footstepAudioClips.Length <= 0)
+        {
+            return;
+        }
+        
+        int index = Random.Range(0, footstepAudioClips.Length);
+        AudioSource.PlayClipAtPoint(footstepAudioClips[index], transform.TransformPoint(_controller.center), footstepAudioVolume);
     }
 
     private void OnLand(AnimationEvent animationEvent)
