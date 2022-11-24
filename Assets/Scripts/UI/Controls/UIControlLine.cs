160d1dfbdb5688f9ae94cbf8f750d3683bad21f0
Ismail Bennani <avace.contact@gmail.com>
2022-11-24 22:12:54 +0100

    add controls ui

---
+++ Assets/Scripts/UI/Controls/UIControlLine.cs
@@ -0,0 +1,19 @@
+﻿using Input;
+using TMPro;
+using UnityEngine;
+
+namespace UI.Controls
+{
+    public class UIControlLine: MonoBehaviour
+    {
+        public TextMeshProUGUI text;
+
+        public void SetFromCallback(GameInputCallback callback)
+        {
+            if (text)
+            {
+                text.SetText(callback.ActionName);
+            }
+        }
+    }
+}
