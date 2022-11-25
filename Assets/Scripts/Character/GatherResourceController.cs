ecb1ba196cf93f5e44a06d69d70ccce2ad845b0e
Ismail Bennani <avace.contact@gmail.com>
2022-11-26 00:57:34 +0100

    add wood sfx

--- Assets/Scripts/Character/GatherResourceController.cs
+++ Assets/Scripts/Character/GatherResourceController.cs
@@ -1,14 +1,24 @@
-﻿using Map;
+﻿using System;
+using System.Collections.Generic;
+using Map;
 using Map.Tile;
 using Resource;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Character
 {
     public class GatherResourceController : MonoBehaviour
     {
+        [Header("Mine")]
         public bool allowMine;
+        public AudioClip[] mineAudioClips;
+        public float mineAudioClipVolume;
+        
+        [Header("Chop")]
         public bool allowChop;
+        public AudioClip[] chopAudioClips;
+        public float chopAudioClipVolume;
         
         private int _animIDMine;
         private int _animIDChop;
@@ -93,10 +103,35 @@ namespace Character
             int actualConsumption = tile.ConsumeResource(1);
             state.resource.Produce(_currentResource, actualConsumption);
 
+            switch (_currentResource)
+            {
+                case ResourceType.None:
+                    throw new InvalidOperationException("Cannot happen");
+                case ResourceType.Wood:
+                    PlayClip(chopAudioClips, chopAudioClipVolume);
+                    break;
+                case ResourceType.Stone:
+                    PlayClip(mineAudioClips, mineAudioClipVolume);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
             if (!tile.HasResource)
             {
                 CancelGather();
             }
+        }
+
+        private void PlayClip(IReadOnlyList<AudioClip> clips, float volume)
+        {
+            if (clips.Count <= 0)
+            {
+                return;
+            }
+
+            int index = Random.Range(0, clips.Count);
+            AudioSource.PlayClipAtPoint(clips[index], transform.position, volume);
         }
     }
 }
